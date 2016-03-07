aa8c548aa4c02d3f7047c6aba2d652728a54495d
Eugene Obukhov <irvind25@gmail.com>
2016-03-07 04:45:45 +0300

    [functest] Checking submit result

--- functional_tests.py
+++ functional_tests.py
@@ -35,18 +35,34 @@ class HomePageTest(unittest.TestCase):
         )
 
         new_item_price_elem.send_keys('100.50')
+        expected_spent = spent - 100.50
 
         # Жмем записать
         self.browser.find_element_by_id('add_new_item').click()
 
         # Должны увидеть наш пункт в списке
-        self.fail('Finish me')
+        first_item = self.browser.find_elements_by_css_selector(
+            '#main_item_box > .item'
+        )[0]
+
+        self.assertEqual(
+            first_item.find_element_by_css_selector('span.item-name').text,
+            'Ненужная штуковина'
+        )
+
+        self.assertEqual(
+            float(first_item.find_element_by_css_selector('span.item-price').text),
+            100.50
+        )
 
         # Должны увидеть что расходы увеличились
-
-        # Повторяем
+        self.assertEqual(
+            float(self.browser.find_element_by_id('spent_amount').text),
+            expected_spent
+        )
 
         # Заходим на сайт позже и расчитываем увидеть наши заполненные данные
+        self.fail('Finish me')
 
 if __name__ == '__main__':
     unittest.main()
