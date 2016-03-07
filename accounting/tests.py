a8d4090c918568887ba7656a4eccb7bf644ec1eb
Eugene Obukhov <irvind25@gmail.com>
2016-03-07 03:54:54 +0300

    Index page tests

--- accounting/tests.py
+++ accounting/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.test.client import Client
 
-# Create your tests here.
+from django.core.urlresolvers import reverse
+
+
+class BaseTestCase(TestCase):
+    def setUp(self):
+        self.c = Client()
+
+
+class IndexPageViewTest(BaseTestCase):
+    def test_index_page_returns_correct_html(self):
+        response = self.c.get(reverse('accounting:index'))
+        self.assertEqual(response.status_code, 200)
+
+        self.assertContains(
+            response,
+            '<h1>Расходы / Доходы</h1>',
+            html=True
+        )
+
+        self.assertContains(
+            response,
+            '<title>Учет расходов</title>',
+            html=True
+        )
+
+        self.assertContains(
+            response,
+            '<div id="main-item-box">',
+            html=True
+        )
