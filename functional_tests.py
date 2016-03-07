afaa528009f040dfa1da4827f32cc86d791d5ed5
Eugene Obukhov <irvind25@gmail.com>
2016-03-07 03:04:29 +0300

    Functional test redone

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,18 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class HomePageTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_home_page(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Расходы / Доходы', self.browser.title)
+
+
+if __name__ == '__main__':
+    unittest.main()
