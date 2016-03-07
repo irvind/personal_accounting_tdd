eb92fb685e65f4c641dc0622267fa7122d40aebb
Eugene Obukhov <irvind25@gmail.com>
2016-03-07 05:14:52 +0300

    Add unit test for item posting

--- accounting/tests.py
+++ accounting/tests.py
@@ -8,6 +8,13 @@ class BaseTestCase(TestCase):
     def setUp(self):
         self.c = Client()
 
+    def assertContainsHtml(self, resp, html):
+        self.assertContains(
+            resp,
+            html,
+            html=True
+        )
+
 
 class IndexPageViewTest(BaseTestCase):
     def test_index_page_returns_correct_html(self):
@@ -30,4 +37,26 @@ class IndexPageViewTest(BaseTestCase):
             response,
             '<div id="main_item_box">',
             html=True
+        )
+
+    def test_index_page_accepts_new_items(self):
+        item_name, item_price = 'Бананы', 70
+
+        response = self.c.post(
+            reverse('accounting:index'),
+            {'name': item_name, 'price': str(item_price)}
+        )
+
+        self.assertRedirects(response, reverse('accounting:index'))
+
+        response = self.c.get(reverse('accounting:index'))
+
+        self.assertContainsHtml(
+            response,
+            '<span class="item-name">%s</span>' % item_name
+        )
+
+        self.assertContainsHtml(
+            response,
+            '<span class="item-price">%s</span>' % (str(item_price) + '.00')
         )
