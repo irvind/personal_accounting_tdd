77be0b24453d4ffe4591bb5e49a1109c7e2a2e5c
Eugene Obukhov <irvind25@gmail.com>
2016-03-12 17:07:34 +0300

    Tests refactoring, new expose view handler

--- accounting/tests.py
+++ accounting/tests.py
@@ -1,7 +1,11 @@
+from unittest import skip
+
 from django.test import TestCase
 from django.test.client import Client
 
 from django.core.urlresolvers import reverse
+
+from accounting.models import Expense
 
 
 class BaseTestCase(TestCase):
@@ -39,11 +43,41 @@ class IndexPageViewTest(BaseTestCase):
             html=True
         )
 
+
+class NewExpenseViewTest(BaseTestCase):
+    def test_creates_new_expense_model(self):
+        item_name, item_price = 'Бананы', 70
+
+        resp = self.c.post(
+            reverse('accounting:new_expense'),
+            {'name': item_name, 'price': item_price}
+        )
+
+        self.assertRedirects(resp, reverse('accounting:index'))
+
+        self.assertEqual(Expense.objects.count(), 1)
+        expense = Expense.objects.first()
+
+        self.assertEqual(expense.name, item_name)
+        self.assertEqual(expense.price, item_price)
+
+    @skip
+    def test_cannot_create_if_name_is_missing(self):
+        resp = self.c.post(
+            reverse('accounting:new_expense'),
+            {'name': 'some name'}
+        )
+
+        self.assertEqual(resp.status_code, 400)
+
+
+class IndexNewExpenseIntegrationTest(BaseTestCase):
+    @skip
     def test_index_page_accepts_new_items(self):
         item_name, item_price = 'Бананы', 70
 
         response = self.c.post(
-            reverse('accounting:index'),
+            reverse('accounting:new_expense'),
             {'name': item_name, 'price': str(item_price)}
         )
 
--- accounting/views.py
+++ accounting/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
-# from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.core.urlresolvers import reverse
+
+from .models import Expense
 
 
 def index_view(request):
     return render(request, 'accounting/index.html')
+
+
+def new_expense_view(request):
+    exp = Expense.objects.create(
+        name=request.POST['name'],
+        price=int(request.POST['price'])
+    )
+    return redirect(reverse('accounting:index'))
