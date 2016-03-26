8f99e588f98caf09040d3aeda0973f01e5b3432c
Eugene Obukhov <irvind25@gmail.com>
2016-03-27 02:29:52 +0300

    Add manager for Expense model and extend it, add tests

--- accounting/models.py
+++ accounting/models.py
@@ -6,10 +6,70 @@ from django.db import models
 from django.db.models import Sum
 
 
+def invert_dict(di):
+    return {
+        v: k for k, v in di.items()
+    }
+
+
+class ExpenseManager(models.Manager):
+    @staticmethod
+    def create_expense(name, price=None, unit='x', amount=1,
+                       unit_price=None, **kwargs):
+        if price is None and unit_price is not None:
+            price = unit_price * amount
+
+        price = round(Decimal(price), 2)
+
+        key = 'countable_amount' if unit == 'x' else 'mesurable_amount'
+        kwargs[key] = amount
+
+        exp = Expense(
+            name=name,
+            price=price,
+            unit_type=unit,
+            unit_price=unit_price,
+            **kwargs
+        )
+        exp.full_clean()
+        exp.save()
+        return exp
+
+
 class Expense(models.Model):
+    UNIT_TYPE_CHOICES = (
+        ('x', 'Неделимые'),
+        ('kg', 'Килограммы'),
+        ('g', 'Граммы'),
+        ('l', 'Литры'),
+        ('ml', 'Миллилитры'),
+    )
+
+    rus_type_choice_mapping = {
+        'kg': 'кг',
+        'g': 'г',
+        'l': 'л',
+        'ml': 'мл',
+    }
+
+    objects = ExpenseManager()
+
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     date = models.DateField(default=datetime.date.today)
+    unit_type = models.CharField(
+        max_length=2,
+        choices=UNIT_TYPE_CHOICES,
+        default='x'
+    )
+    countable_amount = models.IntegerField(null=True, blank=True)
+    mesurable_amount = models.FloatField(null=True, blank=True)
+    unit_price = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+        null=True,
+        blank=True
+    )
     created = models.DateField(auto_now_add=True)
 
     @classmethod
@@ -19,3 +79,34 @@ class Expense(models.Model):
         )['sum']
 
         return -_sum if _sum is not None else Decimal(0)
+
+    @property
+    def amount(self):
+        if self.unit_type == 'x':
+            return self.countable_amount
+
+        return self.mesurable_amount
+
+    @property
+    def quantity_repr(self):
+        # todo: move to template filter
+        if self.unit_type == 'x':
+            return 'x{}'.format(self.countable_amount)
+        else:
+            amount = self.mesurable_amount
+            if amount == int(amount):
+                amount = int(amount)
+
+            return '{} {}'.format(
+                amount,
+                self.rus_type_choice_mapping[self.unit_type]
+            )
+
+    @classmethod
+    def eng_unit_type_to_rus(cls, eng_unit):
+        return cls.rus_type_choice_mapping[eng_unit]
+
+    @classmethod
+    def rus_unit_type_to_eng(cls, rus_unit):
+        di = invert_dict(cls.rus_type_choice_mapping)
+        return di[rus_unit]
--- accounting/tests/test_models.py
+++ accounting/tests/test_models.py
@@ -1,14 +1,17 @@
+from unittest import skip
+
 from datetime import date
 from decimal import Decimal
 
 from django.test import TestCase
+from django.core.exceptions import ValidationError
 
 from accounting.models import Expense
 
 
 class ExpenseModelTest(TestCase):
     def test_can_create_model(self):
-        Expense.objects.create(
+        Expense.objects.create_expense(
             name='Some item',
             price=123.45
         )
@@ -19,7 +22,7 @@ class ExpenseModelTest(TestCase):
         self.assertEqual(exp.date, date.today())
 
     def test_can_create_countable(self):
-        Expense.objects.create_order(
+        Expense.objects.create_expense(
             name='Some item',
             unit='x',
             amount=3,
@@ -28,13 +31,23 @@ class ExpenseModelTest(TestCase):
 
         exp = Expense.objects.last()
         self.assertEqual(exp.name, 'Some item')
-        self.assertEqual(exp.unit, 'x')
+        self.assertEqual(exp.unit_type, 'x')
         self.assertEqual(exp.amount, 3)
         self.assertEqual(exp.unit_price, 60)
         self.assertEqual(exp.price, 180)
 
+    def test_creates_one_countable_by_default(self):
+        Expense.objects.create_expense(
+            name='Some item',
+            price=60
+        )
+
+        exp = Expense.objects.last()
+        self.assertEqual(exp.unit_type, 'x')
+        self.assertEqual(exp.amount, 1)
+
     def test_can_create_measurable(self):
-        Expense.objects.create_order(
+        Expense.objects.create_expense(
             name='Some item',
             unit='kg',
             amount=2.5,
@@ -42,18 +55,49 @@ class ExpenseModelTest(TestCase):
         )
 
         exp = Expense.objects.last()
-        self.assertEqual(exp.unit, 'kg')
+        self.assertEqual(exp.unit_type, 'kg')
         self.assertEqual(exp.amount, 2.5)
         self.assertEqual(exp.unit_price, 100)
         self.assertEqual(exp.price, 250)
 
-        Expense.objects.create_order(
+        Expense.objects.create_expense(
             name='Some item',
             unit='g',
             amount=700,
             unit_price=3
         )
-        self.assertEqual(exp.unit, 'g')
+        exp = Expense.objects.last()
+        self.assertEqual(exp.unit_type, 'g')
+
+    def test_cannot_create_exp_with_blank_name(self):
+        with self.assertRaises(ValidationError):
+            Expense.objects.create_expense(
+                name='',
+                price=200.5
+            )
+
+    def test_cannot_create_exp_with_invalid_unit(self):
+        with self.assertRaises(ValidationError):
+            Expense.objects.create_expense(
+                name='some item',
+                price=200.5,
+                unit='г',
+                amount=200
+            )
+
+    @skip
+    def test_cannot_create_exp_with_negative_or_zero_price(self):
+        with self.assertRaises(ValidationError):
+            Expense.objects.create_expense(
+                name='some item',
+                price=0
+            )
+
+        with self.assertRaises(ValidationError):
+            Expense.objects.create_expense(
+                name='some item',
+                price=-100
+            )
 
     def test_total_expense_returns_correct_price(self):
         Expense.objects.create(name='Some item', price=60.40)
